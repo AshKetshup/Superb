f89ede8d1e11c9620821421d8e9dcefed825583f
Diogo Simões <dsimoes2000@gmail.com>
2020-11-10 09:18:22 +0000

    Generate Android Files + First attempt on Layout

---
+++ Superb/app/src/main/java/pdm/di/ubi/pt/superb/MainActivity.java
@@ -0,0 +1,14 @@
+package pdm.di.ubi.pt.superb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
