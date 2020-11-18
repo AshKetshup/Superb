7418751daa503d4648f9811ffc2fd1ae21d1ea0f
Diogo Simões <dsimoes2000@gmail.com>
2020-11-18 02:45:11 +0000

    Created Dialog Box for "Creation"

---
+++ Superb/app/src/main/java/pdm/di/ubi/pt/superb/CreateDialogBox.java
@@ -0,0 +1,25 @@
+package pdm.di.ubi.pt.superb;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class CreateDialogBox extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View v = inflater.inflate(R.layout.create_dialog_box, null);
+
+        builder.setView(v);
+        return builder.create();
+    }
+}
